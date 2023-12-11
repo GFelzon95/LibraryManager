fe1b4e7b82bdcbb00fdb2c9a3a0a4e466e6f6660
Gian Felzon Pascual <pascual.gianfelzon@wesleyan.edu.ph>
2023-12-11 16:35:32 +0800

    Seeded DocumentTypes table

---
+++ App_Start/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace LibraryManager.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Mapper.CreateMap<Customer, CustomerDto>();
+        }
+    }
+}
---
+++ Migrations/202312110805449_SeedDocumentTypeTable.cs
@@ -0,0 +1,19 @@
+﻿namespace LibraryManager.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class SeedDocumentTypeTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO DocumentTypes (Id, Name) VALUES (1, 'National ID')");
+            Sql("INSERT INTO DocumentTypes (Id, Name) VALUES (2, 'Passport')");
+            Sql("INSERT INTO DocumentTypes (Id, Name) VALUES (3, 'Driver''s License')");
+            Sql("INSERT INTO DocumentTypes (Id, Name) VALUES (4, 'Postal Id')");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
